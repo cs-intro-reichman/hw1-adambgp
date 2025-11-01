34ac77421831242bd8b45f32cb2a8e4ef24544f4
adam bar giora peles <adam.bargiorapeles@post.runi.ac.il>
2025-11-01 15:02:14 +0200

    Completed assignment

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,13 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int)((Math.random()*lim)+ 1);
+		int b = (int)((Math.random()*lim)+ 1);
+		int c = (int)((Math.random()*lim)+ 1);
+		int max = Math.max((Math.max(a,b)),c);
+		int min = Math.min((Math.min(a,b)),c);
+		int mid = (a + b + c) - (min + max);
+		System.out.println( min +" " + mid + " " + max);
 	}
 }
