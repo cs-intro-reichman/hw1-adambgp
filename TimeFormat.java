0389740d576e31192384be97853d54f82dc0066b
adam bar giora peles <adam.bargiorapeles@post.runi.ac.il>
2025-11-02 17:23:58 +0200

    finito

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,7 +12,7 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        String min= " ";
+        String min = String.valueOf(minutes);
 		if (minutes<10){
 				min = "0" + minutes;
 			}
